d8d847ffc265dd61a80fd447b9e91b34b6f48828
yknli <augustus0613@hotmail.com>
2024-12-28 17:54:10 +0800

    test: add tests of failed scenarios for configure method

--- spec/pxpayplus_spec.rb
+++ spec/pxpayplus_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe Pxpayplus do
       expect(Pxpayplus.secret_key).to eq('key')
       expect(Pxpayplus.merchant_code).to eq('code')
     end
+
+    it 'raise an error if no configuration is given' do
+      expect { Pxpayplus.configure }.to raise_error(RuntimeError, 'Please use configure method to setup your pxpayplus credentials.')
+    end
+
+    it 'raise an error if secret_key not set' do
+      expect { Pxpayplus.configure{ |config| } }.to raise_error(RuntimeError, 'secret_key not set.')
+    end
+
+    it 'raise an error if merchant_code not set' do
+      expect { Pxpayplus.configure{ |config| config.secret_key = 'key' } }.to raise_error(RuntimeError, 'merchant_code not set.')
+    end
   end
 end
