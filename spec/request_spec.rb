cb46cadebe83a33d0cd60fd2460998e5c2cb7852
yknli <augustus0613@hotmail.com>
2024-12-28 22:31:21 +0800

    test: add test of correctly signing request signature

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe Pxpayplus::Request do
+
+  before(:all) do
+    Pxpayplus.configure do |config|
+      config.secret_key = 'test_secret_key'
+      config.merchant_code = 'test_merchant_code'
+    end
+  end
+
   describe 'initialize' do
     subject(:request) { Pxpayplus::Request.new(params: params, method: method, url: url) }
 
@@ -15,4 +23,18 @@ RSpec.describe Pxpayplus::Request do
     end
   end
 
+  context 'when request initialized' do
+    let(:params) { { auth_binding_no: 'auth_binding_no', req_time: 'req_time' } }
+    let(:method) { :get }
+    let(:url) { 'https://example.com' }
+
+    let(:request) { Pxpayplus::Request.new(params: params, method: method, url: url) }
+
+    describe 'signature' do
+      subject(:signature) { request.signature }
+      it 'signs signature by params' do
+        expect(signature).to eq ('E458D99A038266FA090CEE7890EE48A2B6FB805F403449859CD7DA62D5415ECB')
+      end
+    end
+  end
 end
