eb2ba2698b2a607b9b80496e5399b93d3202201a
yknli <augustus0613@hotmail.com>
2024-12-28 17:53:52 +0800

    test: add test of success scenario for configure method

--- spec/pxpayplus_spec.rb
+++ spec/pxpayplus_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe Pxpayplus do
     expect(Pxpayplus::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'configure' do
+    it "allows configuration through a block" do
+      Pxpayplus.configure do |config|
+        config.secret_key = 'key'
+        config.merchant_code = 'code'
+      end
+
+      expect(Pxpayplus.secret_key).to eq('key')
+      expect(Pxpayplus.merchant_code).to eq('code')
+    end
   end
 end
