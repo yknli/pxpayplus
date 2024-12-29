8f568110898c0626dfe55df7ad63ddb06a02ee45
yknli <augustus0613@hotmail.com>
2024-12-30 00:20:16 +0800

    test: add test cases to test action_to_request_klass_name method

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe Pxpayplus::Client do
     end
   end
 
+  describe 'action_to_request_klass_name' do
+    it 'returns request class name for given action' do
+      klass_name = client.send(:action_to_request_klass_name, :create_auth_order)
+      expect(klass_name).to eq('Pxpayplus::RequestDefinition::CreateAuthOrderRequest')
+
+      klass_name = client.send(:action_to_request_klass_name, :debit)
+      expect(klass_name).to eq('Pxpayplus::RequestDefinition::DebitRequest')
+
+      klass_name = client.send(:action_to_request_klass_name, :unbind)
+      expect(klass_name).to eq('Pxpayplus::RequestDefinition::UnbindRequest')
+
+      klass_name = client.send(:action_to_request_klass_name, :refund)
+      expect(klass_name).to eq('Pxpayplus::RequestDefinition::RefundRequest')
+
+      klass_name = client.send(:action_to_request_klass_name, :check_order_status)
+      expect(klass_name).to eq('Pxpayplus::RequestDefinition::CheckOrderStatusRequest')
+
+      klass_name = client.send(:action_to_request_klass_name, :update_debit_time)
+      expect(klass_name).to eq('Pxpayplus::RequestDefinition::UpdateDebitTimeRequest')
+    end
+  end
+
 end
