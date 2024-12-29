9740149899bee6e6d432af1e08a1c02f1dcc0201
yknli <augustus0613@hotmail.com>
2024-12-29 23:57:10 +0800

    feat: add action_to_request_klass_name method to pxpayplus client

--- lib/pxpayplus/client.rb
+++ lib/pxpayplus/client.rb
@@ -3,5 +3,16 @@ module Pxpayplus
     def actions
       [ :create_auth_order, :debit, :unbind, :refund, :check_order_status, :update_debit_time ]
     end
+
+    private
+
+    # Converts a symbol of action name to request class name
+    # @param [Object] api action name
+    # @return [String] request class name
+    def action_to_request_klass_name(action)
+      klass_name = "#{action.to_s}_request"
+      klass_name = klass_name.split('_').collect(&:capitalize).join
+      "Pxpayplus::RequestDefinition::#{klass_name}"
+    end
   end
 end
